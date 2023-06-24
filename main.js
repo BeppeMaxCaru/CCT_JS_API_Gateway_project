2dd9f711ac3888c20ea845bf34699ba1e1f39162
BeppeMaxCaru <79905424+BeppeMaxCaru@users.noreply.github.com>
2023-06-24 17:11:30 +0200

    Third commit

--- main.js
+++ main.js
@@ -3,13 +3,43 @@ const { get } = require('http')
 const app = express()
 const port = 3000
 
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }));
+
 //routes
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, 'Front-end/welcome page.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Front-end/login page.html'))
+})
+
+app.post('/login', (req, res) => {
+
+    //console.log(req.body)
+
+    // Retrieve the username and password from the form data
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Perform validation or authentication logic
+    // ...
+
+    // Example: Check if username and password are valid
+    if (username === 'admin' && password === 'password') {
+        // Successful login
+        res.sendFile(path.join(__dirname, 'Front-end/home.html'))
+    } else {
+        // Invalid credentials
+        res.send('Invalid username or password');
+    }
 })
 
 app.get('/test', (req, res) => {
-    res.send('Test page!')
+    //res.send('Test page!')
+    res.sendFile(path.join(__dirname, 'Front-end/home.html'))
 })
 
 app.listen(port, () => {
