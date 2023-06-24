782a53574488702f87e165a00a45a1475beb1749
BeppeMaxCaru <79905424+BeppeMaxCaru@users.noreply.github.com>
2023-06-24 14:38:35 +0200

    Second commit

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/test', (req, res) => {
+    res.send('Test page!')
+})
+
 app.listen(port, () => {
-    console.log('Example app listening on port ${port}')
+    console.log('Example app listening on port ' + port)
 })
