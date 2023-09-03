a9f254445f56980cc8bf9e2cead6ee344c6ce1a5
BeppeMaxCaru <79905424+BeppeMaxCaru@users.noreply.github.com>
2023-09-03 22:27:47 +0200

    Cleaned servers for presentation

--- Backend server 1/server1.js
+++ Backend server 1/server1.js
@@ -3,10 +3,10 @@ const fs = require('fs')
 const https = require('https')
 
 const app = express();
-// Start the server
+//Start the server
 const port = 3001;
 
-// Enable CORS to allow requests from different origins (including the load balancer)
+//Enable CORS to allow requests from different origins (including the load balancer)
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -14,24 +14,24 @@ app.use((req, res, next) => {
   next();
 });
 
-// Define your routes and logic here
+//Define your routes and logic here
 app.get('/', (req, res) => {
   res.send('Hello from Backend Server 3001!');
 });
 
-// Define the heartbeat route
+//Define the heartbeat route
 app.get('/heartbeat', (req, res) => {
   res.send('Backend Server is healthy!');
 });
 
-// Function to start the server with HTTPS
+//Function to start the server with HTTPS
 function startServer(port, options) {
   https.createServer(options, app).listen(port, () => {
     console.log(`Backend Server is running on port ${port} with HTTPS`);
   });
 }
 
-// Load the SSL/TLS certificates
+//Load the SSL/TLS certificates
 const options = {
   key: fs.readFileSync('SecurityServer1/private_key_server1.pem'),
   cert: fs.readFileSync('SecurityServer1/certificate_server1.pem'),
@@ -41,5 +41,5 @@ const options = {
   //By default they accept only the ones signed by official Certification Authority (CA)
 };
 
-// Start the server
+//Start the server
 startServer(port, options);
